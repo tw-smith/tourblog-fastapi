dd7b10fa42e524b571eb43ee9984535f1ca59aeb
tw-smith <92607103+tw-smith@users.noreply.github.com>
2025-09-16 21:11:29 +0100

    feat: update requirements and add CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import posts, photos
 
 app = FastAPI()
+
+origins = [
+    "https://www.cycling-south.com",
+    "https://cycling-south.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(posts.router)
 app.include_router(photos.router)
